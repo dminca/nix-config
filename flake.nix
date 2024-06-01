6d2ec2af0d9df45ddbb41f15ed11c04a157d85aa
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2024-06-01 21:32:48 +0200

    feat: split flake into hosts
    - darwinConfigurations to account for multiple hosts
    - homeManagerConfiguration to expand for multiple hosts
    - define a common set of dependencies and settings which are imported
    
    Resolves: #1 #4
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- flake.nix
+++ flake.nix
@@ -18,22 +18,27 @@
   outputs = { self, nix-darwin, nixpkgs, home-manager, sops-nix }:
   {
     system.configurationRevision = self.rev or self.dirtyRev or null;
-    darwinConfigurations."ne0byte" = nix-darwin.lib.darwinSystem {
-      modules = [ ./system.nix ];
+    darwinConfigurations = {
+      "ne0byte" = nix-darwin.lib.darwinSystem {
+        modules = [
+          ./hosts/MacbookAir.fritz.box/system.nix
+        ];
+      };
     };
 
     darwinPackages = self.darwinConfigurations."ne0byte".pkgs;
 
-    defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
+    # TODO: do we need this?
+    #defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
     homeConfigurations = {
-      "dminca" = home-manager.lib.homeManagerConfiguration {
+      "MacbookAir.fritz.box" = home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs { system = "aarch64-darwin"; };
         modules = [
           sops-nix.homeManagerModules.sops
-          ./home.nix
-          ./vim.nix
-          ./git.nix
-          ./zshrc.nix
+          ./hosts/MacbookAir.fritz.box/home.nix
+          ./hosts/MacbookAir.fritz.box/vim.nix
+          ./hosts/MacbookAir.fritz.box/git.nix
+          ./hosts/MacbookAir.fritz.box/zshrc.nix
         ];
       };
     };

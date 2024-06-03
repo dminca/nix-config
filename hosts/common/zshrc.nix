f79f97b43350bf0f9788896a4038620be392dd2a
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2024-06-03 09:23:12 +0200

    fix(zsh): hardcoded Homebrew bin path
    - avoid hardcoding absolute paths within the configuration because not
      all systems are identical
    
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- hosts/common/zshrc.nix
+++ hosts/common/zshrc.nix
@@ -28,7 +28,7 @@
       setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
       setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
       setopt HIST_BEEP                 # Beep when accessing nonexistent history.
-      eval $(/opt/homebrew/bin/brew shellenv) # Set PATH, MANPATH, etc., for Homebrew.
+      eval "$($(which brew) shellenv)" # Set PATH, MANPATH, etc., for Homebrew.
     '';
     shellAliases = {
       g = "git";

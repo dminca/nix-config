d3fdc876aa36c7df7e5670457e6aaa9ccb6247ef
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2024-06-04 14:29:09 +0200

    feat(vim): beautify lualine
    * show tab numbers
    * show relative path + filename
    
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- hosts/common/neovim.nix
+++ hosts/common/neovim.nix
@@ -63,6 +63,40 @@
         options = {
           theme  = 'nord',
         },
+        sections = {
+          lualine_b = {
+            {
+              'diagnostics',
+              sources = { 'nvim_diagnostic', 'coc' },
+              sections = { 'error', 'warn', 'info', 'hint' },
+              diagnostics_color = {
+                error = 'DiagnosticError',
+                warn  = 'DiagnosticWarn',
+                info  = 'DiagnosticInfo',
+                hint  = 'DiagnosticHint',
+              },
+              symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
+              colored = true,
+            },
+          },
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            },
+          },
+        },
+        inactive_sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            },
+          },
+        },
+        tabline = {
+          lualine_a = {'tabs'},
+        },
       }
     '';
   };

6beefcc371e18e1366dc48cbd980d61c248dfedc
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2024-06-05 22:33:04 +0200

    feat(hm): install ripgrep
    * install `ripgrep` and add some sane defaults as per documentation
      https://github.com/BurntSushi/ripgrep/blob/master/GUIDE.md#configuration-file
    
    * for more information about ripgrep
      https://github.com/BurntSushi/ripgrep
    
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -109,5 +109,17 @@
       ".hg"
     ];
   };
+  programs.ripgrep = {
+    enable = true;
+    arguments = [
+      "--max-columns=150"
+      "--max-columns-preview"
+      "--hidden"
+      "--glob=!.git/*"
+      "--colors=line:none"
+      "--colors=line:style:bold"
+      "--smart-case"
+    ];
+  };
 }
 

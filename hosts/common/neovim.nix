074aea5fcd819e12c0289cbfbcf5f33993e8e534
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2024-06-05 22:34:40 +0200

    feat(vim): install telescope
    * install `telescope` plugin
    * set some sane defaults as documented in the project homepage
      https://github.com/nvim-telescope/telescope.nvim
    
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- hosts/common/neovim.nix
+++ hosts/common/neovim.nix
@@ -53,6 +53,7 @@
       coc-explorer
       nord-nvim
       lualine-nvim
+      telescope-nvim
     ];
     extraLuaConfig = ''
       require'nvim-treesitter.configs'.setup {
@@ -99,6 +100,11 @@
           lualine_a = {'tabs'},
         },
       }
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     '';
   };
 }

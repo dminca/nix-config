3faf53c7bf9816e819cb5d1e2392f5a19d3d5b54
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2024-06-27 20:37:29 +0200

    feat(mba): enable Homebrew
    * seems this operation also took care of cleaning everything which
      wasn't track in this file, which is awesome!
    
    Installing firefox
    Homebrew Bundle complete! 2 Brewfile dependencies now installed.
    ==> Uninstalling Cask maccy
    ==> Backing App 'Maccy.app' up to '/opt/homebrew/Caskroom/maccy/0.29.2/Maccy.app'
    ==> Removing App '/Applications/Maccy.app'
    ==> Purging files for version 0.29.2 of Cask maccy
    ==> Uninstalling Cask mactex
    ==> Uninstalling packages with sudo; the password may be necessary:
    org.tug.mactex.gui2023
    org.tug.mactex.texlive2023
    ==> Removing files:
    /usr/local/texlive/2023
    /Applications/TeX
    /Library/TeX
    /etc/paths.d/TeX
    /etc/manpaths.d/TeX
    ==> Purging files for version 2023.0314 of Cask mactex
    ==> Uninstalling Cask xquartz
    ==> Removing launchctl service org.xquartz.privileged_startx
    ==> Uninstalling packages with sudo; the password may be necessary:
    org.xquartz.X11
    ==> Purging files for version 2.8.1 of Cask xquartz
    ==> Autoremoving 13 unneeded formulae:
    fontconfig
    freetype
    ghostscript
    jbig2dec
    jpeg-turbo
    libidn
    libpng
    libtiff
    little-cms2
    lz4
    openjpeg
    xz
    zstd
    Uninstalling /opt/homebrew/Cellar/ghostscript/10.02.1... (639 files, 151.3MB)
    Uninstalling /opt/homebrew/Cellar/openjpeg/2.5.0_1... (536 files, 13.8MB)
    Uninstalling /opt/homebrew/Cellar/fontconfig/2.15.0... (90 files, 2.4MB)
    Uninstalling /opt/homebrew/Cellar/jbig2dec/0.20... (13 files, 422.7KB)
    Uninstalling /opt/homebrew/Cellar/libidn/1.41... (73 files, 1MB)
    Uninstalling /opt/homebrew/Cellar/little-cms2/2.15... (21 files, 1.4MB)
    Uninstalling /opt/homebrew/Cellar/freetype/2.13.2... (67 files, 2.4MB)
    Uninstalling /opt/homebrew/Cellar/libtiff/4.6.0... (414 files, 6.7MB)
    Uninstalling /opt/homebrew/Cellar/libpng/1.6.40... (27 files, 1.3MB)
    Uninstalling /opt/homebrew/Cellar/zstd/1.5.5... (31 files, 2.1MB)
    Uninstalling /opt/homebrew/Cellar/jpeg-turbo/3.0.0... (44 files, 3.4MB)
    Uninstalling /opt/homebrew/Cellar/lz4/1.9.4... (22 files, 681.4KB)
    Uninstalling /opt/homebrew/Cellar/xz/5.4.5... (163 files, 2.6MB)
    Uninstalled 3 casks
    Uninstalling gettext... (2,042 files, 24.3MB)
    Uninstalling pcre2... (230 files, 6.2MB)
    Uninstalling git... (1,638 files, 50.5MB)
    Uninstalling git-lfs... (78 files, 13.1MB)
    Uninstalled 17 formulae
    Untapping simnalamburt/x...
    Untapped 7 formulae (166 files, 148.4KB).
    Untapping homebrew/services...
    Untapped 1 command (1,402 files, 1.5MB).
    Untapping charmbracelet/tap...
    Untapped 12 formulae (235 files, 208.1KB).
    Untapping wez/wezterm...
    Untapped 2 casks (122 files, 82.7KB).
    Removing: /opt/homebrew/Library/Homebrew/vendor/portable-ruby/3.1.4... (2,150 files, 35.9MB)
    Removing: /opt/homebrew/Library/Homebrew/vendor/portable-ruby/3.3.1... (2,382 files, 41MB)
    ==> This operation has freed approximately 77MB of disk space.
    
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- hosts/MacbookAir.fritz.box/system.nix
+++ hosts/MacbookAir.fritz.box/system.nix
@@ -3,4 +3,19 @@
 }:
 {
   nixpkgs.hostPlatform = "aarch64-darwin";
+  homebrew = {
+    enable = true;
+    onActivation = {
+      cleanup = "uninstall";
+      autoUpdate = true;
+      upgrade = true;
+    };
+    casks = [
+      "notion"
+      {
+        name = "firefox";
+        greedy = true;
+      }
+    ];
+  };
 }

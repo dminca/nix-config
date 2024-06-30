454351d9b9af0fe2fb994822a09a958d447bd5d6
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2024-06-30 22:25:18 +0200

    fix(kitty): jump words by Option+Arrow keys
    * can only reproduce this issue on this machine (Intel), however, adding
      this to the shell enables word jump by Option+Arrow Left/Right
    * found this here:
      * https://www.reddit.com/r/KittyTerminal/comments/us4mol/how_can_i_map_option_left_arrow_and_option_right/
      * how to enable it on Nix
        * https://nixos.wiki/wiki/Zsh
    
    Resolves:
    Related: kovidgoyal/kitty#838
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- hosts/M-C02FX3JUML85/zshrc.nix
+++ hosts/M-C02FX3JUML85/zshrc.nix
@@ -7,6 +7,9 @@
     initExtra = ''
       gpg-connect-agent updatestartuptty /bye > /dev/null
       export VAULT_ADDR="https://$(cat ${config.sops.secrets.sman.path})";
+      # kitty; jump words
+      bindkey "\e[1;3D" backward-word # Option+Left
+      bindkey "\e[1;3C" forward-word # Option+Right
     '';
     shellAliases = {
       k = "kubectl";

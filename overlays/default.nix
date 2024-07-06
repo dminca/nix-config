ab18a2f0472753673d2164cd79c986e982c91d4e
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2024-07-07 00:52:33 +0200

    fix: drop the first lines
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,3 @@
-# import all nix files in the current folder, and execute them with args as parameters
-# The return value is a list of all execution results, which is the list of overlays
-
 args:
 # execute and import all overlay files in the current directory with the given args
 builtins.map

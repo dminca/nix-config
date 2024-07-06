1abf8c161219d7eae00ffbcd60ff9ecdee047724
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2024-07-07 01:12:37 +0200

    fix: override buildGoModule
    * applied Kevin's trick (thanks Kevin!)
      * https://kevinmacksa.me/post/20240128-nix-develop/
    
    error:
           … while evaluating a branch condition
    
             at /nix/store/7gmkl9bfx98djaamhym69wkj4r3sh1q6-source/lib/lists.nix:125:9:
    
              124|       fold' = n:
              125|         if n == len
                 |         ^
              126|         then nul
    
           … while calling the 'length' builtin
    
             at /nix/store/7gmkl9bfx98djaamhym69wkj4r3sh1q6-source/lib/lists.nix:123:13:
    
              122|     let
              123|       len = length list;
                 |             ^
              124|       fold' = n:
    
           (stack trace truncated; use '--show-trace' to show the full trace)
    
           error: function 'anonymous lambda' called without required argument 'pkgs'
    
           at /nix/store/689c81khgfny2578misrv97mhf1m1hk6-source/overlays/kluctl/default.nix:1:1:
    
                1| { pkgs, config, lib, ... }:
                 | ^
                2|
    
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- overlays/kluctl/default.nix
+++ overlays/kluctl/default.nix
@@ -1,12 +1,16 @@
 { pkgs, config, lib, ... }:
 
-(self: super: {
-  kluctl = super.kluctl.overrideAttrs {
-    src = pkgs.fetchFromGitHub {
-      owner = "kluctl";
-      repo = "kluctl";
-      rev = "v2.25.0";
-      hash = "";
-    };
+(final: prev: {
+  kluctl = prev.kluctl.override {
+    buildGoModule = previousArgs: prev.buildGoModule (previousArgs // rec {
+      version = "2.25.0";
+      src = prev.fetchFromGitHub {
+        owner = "kluctl";
+        repo = "kluctl";
+        rev = "refs/tags/v${version}";
+        hash = "";
+        };
+    });
   };
 })
+

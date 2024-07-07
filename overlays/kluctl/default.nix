58080e5f7930bf044d97120128ce1adcbabdc038
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2024-07-07 16:08:25 +0200

    fix: override the vendorHash
    * this is a terrible hack but unfortunately it's necessary because
      buildGoModule computes the vendorHash after installing all Go modules
    * although overriding the vendorHash works just fine, there's still
      something wrong because the overlay is not applied properly, as per
    
        kluctl --version
        kluctl version v2.22.1
    
    Resolves:
    Related: NixOS/nixpkgs#86349
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- overlays/kluctl/default.nix
+++ overlays/kluctl/default.nix
@@ -4,6 +4,7 @@
   kluctl = prev.kluctl.override {
     buildGoModule = previousArgs: let self = prev.buildGoModule (previousArgs // {
       version = "2.25.0";
+      vendorHash = "sha256-TckT39wQn4dclcYSfxootv1Lw5+iYxY6/wwdUc1+Z6s=";
       src = prev.fetchFromGitHub {
         owner = "kluctl";
         repo = "kluctl";

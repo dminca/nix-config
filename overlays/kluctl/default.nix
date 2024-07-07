0dd38f54aa9fbaa3a2fdc6f7f95227f590d03d4d
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2024-07-07 10:54:00 +0200

    fix: replace rec with let-in
    * `rec` is slow and has a lot of downsides; replace with `let-in`
      expression to avoid other strange issues what I was facing, for
      instance this error was thrown previously
    
    error: hash mismatch in fixed-output derivation '/nix/store/yw5l1d80v05y5bgdmc2yz4r1s2hqfvi7-kluctl-2.25.0-go-modules.drv':
             specified: sha256-EEOVd15f1SK8InSIG+TuVwWibkf+ePJ5AGZpiMD+RaQ=
                got:    sha256-TckT39wQn4dclcYSfxootv1Lw5+iYxY6/wwdUc1+Z6s=
    error: 1 dependencies of derivation '/nix/store/f2dpnsp3zh52i5l6iaafrq45wc9370k6-kluctl-2.25.0.drv' failed to build
    error: 1 dependencies of derivation '/nix/store/f98mgwkpz9mq6cf8h34533khik8vy7iq-home-manager-applications.drv' failed to build
    error: 1 dependencies of derivation '/nix/store/lqx96ba7yjswfjfmsljb370ppci3hsch-home-manager-fonts.drv' failed to build
    error: 1 dependencies of derivation '/nix/store/vrssavvbiyjbiq4yb2psd9yis8zjlyyv-home-manager-path.drv' failed to build
    error: 1 dependencies of derivation '/nix/store/1cdk98lgzjyybirwq318zcxb81zlbdzh-home-manager-generation.drv' failed to build
    
    * this error is still present with the `let-in` expression ...
    
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- overlays/kluctl/default.nix
+++ overlays/kluctl/default.nix
@@ -2,15 +2,15 @@
 
 (final: prev: {
   kluctl = prev.kluctl.override {
-    buildGoModule = previousArgs: prev.buildGoModule (previousArgs // rec {
+    buildGoModule = previousArgs: let self = prev.buildGoModule (previousArgs // {
       version = "2.25.0";
       src = prev.fetchFromGitHub {
         owner = "kluctl";
         repo = "kluctl";
-        rev = "refs/tags/v${version}";
+        rev = "refs/tags/v${self.version}";
         hash = "sha256-WtTBkc9mop+bfMcVLI8k4Bqmift5JG9riF+QbDeiR9c=";
         };
-    });
+    }); in self;
   };
 })
 

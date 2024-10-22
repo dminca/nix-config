8f0dc2ebfd72694d7272953f1bc9aba6762f55e9
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2024-10-22 09:28:58 +0200

    Revert "fix(term): remove 'kitty'"
    This reverts commit c316a790f897ff19806750dae38274b905d40e77.

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -123,6 +123,18 @@
       "--smart-case"
     ];
   };
+  programs.kitty = {
+    enable = true;
+    font.name = "JetBrainsMono";
+    themeFile = "Catppuccin-Mocha";
+    extraConfig = ''
+      tab_bar_min_tabs            1
+      tab_bar_edge                bottom
+      tab_bar_style               powerline
+      tab_powerline_style         slanted
+      tab_title_template          {title}{' :{}:'.format(num_windows) if num_windows > 1 else ''\}
+    '';
+  };
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;

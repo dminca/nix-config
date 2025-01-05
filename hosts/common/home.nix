a726ada22b2b3432e0c02e0d02826c96a5194bc7
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2025-01-05 12:06:07 +0100

    fix(alacritty): deprecated config reload flag
    * turns out this flag sits now under 'general.*'; adjust and reapply
    
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -173,7 +173,7 @@
   programs.alacritty = {
     enable = true;
     settings = {
-      live_config_reload = true;
+      general.live_config_reload = true;
       colors.draw_bold_text_with_bright_colors = true;
       window = {
         blur = true;

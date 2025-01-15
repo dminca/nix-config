ce69bd77aa2b5aa28317ef1f20f4a7b8f889614e
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2025-01-15 12:35:38 +0100

    fix(kitty): remove title bar
    * title bar is really annoying and looks horendously expecially when I'm
      trying to NOT FULLSCREEN IT but rather horizontally-split run the app
      and work inside it just like that
    
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -135,6 +135,7 @@
       tab_bar_style               powerline
       tab_powerline_style         slanted
       tab_title_template          {title}{' :{}:'.format(num_windows) if num_windows > 1 else ''\}
+      hide_window_decorations     titlebar-only
     '';
   };
   programs.yazi = {

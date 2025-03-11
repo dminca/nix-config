347aeebb32bee02fc2db32812cb33c1c00340ada
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2025-03-11 15:29:37 +0100

    fix(work): screen saver and lock settings
    * toggle these settings in System Settings
      * Lock Screen > Turn display off on power adapter when inactive =
        Never
      * Lock Screen > Require password after screen saver begins or display
        is turned off = After 4 hours
      * https://www.perplexity.ai/search/how-can-i-set-the-macos-settin-0xrhRLeAR_iuCIkvzro6NA#1
    
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- hosts/MLGERC02FX3JUML85/system.nix
+++ hosts/MLGERC02FX3JUML85/system.nix
@@ -3,6 +3,11 @@
 }:
 {
   nixpkgs.hostPlatform = "x86_64-darwin";
+  system.defaults.NSGlobalDomain.NSIdleDisplaySleepInterval = 0;
+  system.defaults.screensaver = {
+    askForPassword = true;
+    askForPasswordDelay = 14400;
+  };
   homebrew = {
     casks = [
       "slack"

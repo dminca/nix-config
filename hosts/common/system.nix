71c5c3758a02e2ed1097064fb719b281ce4d1794
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2025-03-11 15:21:08 +0100

    fix: app expose trackpad
    * this toggles this setting in System Settings > Trackpad
      * App Expose > Swipe Down with Three Fingers
      * https://www.perplexity.ai/search/how-can-i-set-the-macos-settin-0xrhRLeAR_iuCIkvzro6NA
    
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- hosts/common/system.nix
+++ hosts/common/system.nix
@@ -15,6 +15,7 @@
   system.keyboard.remapCapsLockToEscape = true;
   system.defaults.NSGlobalDomain.InitialKeyRepeat = 14;
   system.defaults.NSGlobalDomain.KeyRepeat = 1;
+  system.defaults.trackpad.TrackpadThreeFingerVertSwipeGesture = 2;
   homebrew = {
     enable = true;
     onActivation = {

93d9ce5184166fc2b568c24d9167e911e70977b5
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2025-03-29 23:12:57 +0100

    feat: enable 'tmux' in Alacritty
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -211,6 +211,14 @@
           chars = "\\u001BB";
         }
       ];
+      terminal.shell.program = lib.getExe pkgs.tmux;
+      terminal.shell.args = [
+        "new-session"
+        "-A"
+        "-D"
+        "-s"
+        "main"
+      ];
     };
   };
 }

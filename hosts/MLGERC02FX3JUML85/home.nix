e68bf7f64a8bf73b2a4d77d3a7cf8273c04aff50
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2025-04-11 09:44:09 +0200

    fix(pkgs): install 'gke-gcloud-auth-plugin'
    * this is required by Google Cloud SDK for GKE
    
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- hosts/MLGERC02FX3JUML85/home.nix
+++ hosts/MLGERC02FX3JUML85/home.nix
@@ -37,6 +37,7 @@
     helm-docs
     fx
     google-cloud-sdk
+    (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
     opentofu
     terraform
     ########

2d381fda19f84554daa9be7b708d1994e5babe17
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2025-04-18 16:49:13 +0200

    feat(pkgs): install 'lazygit'
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -250,4 +250,15 @@
       ];
     };
   };
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui.theme = {
+        lightTheme = true;
+        activeBorderColor = [ "blue" "bold" ];
+        inactiveBorderColor = [ "black" ];
+        selectedLineBgColor = [ "default" ];
+      };
+    };
+  };
 }

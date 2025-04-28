aac7881dc671309908464c48fbda385f2eceda04
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2025-04-28 17:49:33 +0200

    fix(zsh): deprecated field 'initExtra'

--- hosts/MLGERC02FX3JUML85/zshrc.nix
+++ hosts/MLGERC02FX3JUML85/zshrc.nix
@@ -4,7 +4,7 @@
 }:
 {
   programs.zsh = {
-    initExtra = ''
+    initContent = ''
       gpg-connect-agent updatestartuptty /bye > /dev/null
       export VAULT_ADDR="https://$(cat ${config.sops.secrets.sman.path})";
       # kitty; jump words

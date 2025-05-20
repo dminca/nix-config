af7f2c1cd6890560573f5112dffba35db0add933
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2025-05-20 11:06:11 +0200

    fix(flake): format and conditional for user selection
    * running the flake previously was complaining that
      Error: USER is set to "mida4001" but we expect "dminca"
    * as a result, adjust the logic by introducing a boolean for selecting
      the correct user on the workstation
    * format the code according to Nix standards
    
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- flake.nix
+++ flake.nix
@@ -15,57 +15,68 @@
     };
   };
 
-  outputs = { self, nix-darwin, nixpkgs, home-manager, sops-nix }: let
-    # Define darwinConfigurations first
-    darwinConfigurations = {
-      "ZionProxy" = nix-darwin.lib.darwinSystem {
-        modules = [
-          ./hosts/common/system.nix
-          ./hosts/ZionProxy/system.nix
-        ];
+  outputs = { self, nix-darwin, nixpkgs, home-manager, sops-nix }:
+    let
+      # Define darwinConfigurations first
+      darwinConfigurations = {
+        "ZionProxy" = nix-darwin.lib.darwinSystem {
+          modules = [
+            ./hosts/common/system.nix
+            ./hosts/ZionProxy/system.nix
+          ];
+        };
+
+        "MLGERHL6W4P2RXH" = nix-darwin.lib.darwinSystem {
+          modules = [
+            ./hosts/common/system.nix
+            ./hosts/MLGERHL6W4P2RXH/system.nix
+          ];
+        };
       };
 
-      "MLGERHL6W4P2RXH" = nix-darwin.lib.darwinSystem {
-        modules = [
-          ./hosts/common/system.nix
-          ./hosts/MLGERHL6W4P2RXH/system.nix
-        ];
+      homeConfigurations = {
+        "dminca" = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs { system = "aarch64-darwin"; };
+          modules = [
+            sops-nix.homeManagerModules.sops
+            ./hosts/common
+            ./hosts/ZionProxy
+          ];
+        };
+
+        "mida4001" = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs { system = "aarch64-darwin"; };
+          modules = [
+            sops-nix.homeManagerModules.sops
+            ./hosts/common
+            ./hosts/MLGERHL6W4P2RXH
+          ];
+        };
       };
+    in
+    {
+      # Expose darwinConfigurations and homeConfigurations
+      inherit darwinConfigurations homeConfigurations;
+
+      # Define packages for `nix build`
+      packages.aarch64-darwin.default =
+        let
+          pkgs = import nixpkgs { system = "aarch64-darwin"; };
+        in
+        pkgs.writeShellScriptBin "apply-configurations" ''
+          USERNAME=$(whoami)
+
+          if [ "$USERNAME" = "dminca" ]; then
+            ${darwinConfigurations.ZionProxy.config.system.build.toplevel}/sw/bin/darwin-rebuild switch --flake . &&
+            ${homeConfigurations.dminca.activationPackage}/activate
+          elif [ "$USERNAME" = "mida4001" ]; then
+            ${darwinConfigurations.MLGERHL6W4P2RXH.config.system.build.toplevel}/sw/bin/darwin-rebuild switch --flake . &&
+            ${homeConfigurations.mida4001.activationPackage}/activate
+          else
+            echo "Unknown user: $USERNAME"
+            exit 1
+          fi
+        '';
     };
-
-    homeConfigurations = {
-      "dminca" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs { system = "aarch64-darwin"; };
-        modules = [
-          sops-nix.homeManagerModules.sops
-          ./hosts/common
-          ./hosts/ZionProxy
-        ];
-      };
-
-      "mida4001" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs { system = "aarch64-darwin"; };
-        modules = [
-          sops-nix.homeManagerModules.sops
-          ./hosts/common
-          ./hosts/MLGERHL6W4P2RXH
-        ];
-      };
-    };
-  in {
-    # Expose darwinConfigurations and homeConfigurations
-    inherit darwinConfigurations homeConfigurations;
-
-    # Define packages for `nix build`
-    packages.aarch64-darwin.default = let
-      pkgs = import nixpkgs { system = "aarch64-darwin"; };
-    in pkgs.writeShellScriptBin "apply-configurations" ''
-      ${darwinConfigurations.ZionProxy.config.system.build.toplevel}/sw/bin/darwin-rebuild switch --flake . &&
-      ${homeConfigurations.dminca.activationPackage}/activate
-
-      ${darwinConfigurations.MLGERHL6W4P2RXH.config.system.build.toplevel}/sw/bin/darwin-rebuild switch --flake . &&
-      ${homeConfigurations."mida4001".activationPackage}/activate
-    '';
-  };
 }
 

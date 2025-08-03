b574b4aaa59781d085b606530fc33b5e11209c1f
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2025-08-03 16:03:41 +0200

    Revert "fix(vim): set theme to 'catppuccin-latte'"
    This reverts commit 8b977d46a781644ba08f2db318823f377d2b261d.

--- hosts/common/neovim.nix
+++ hosts/common/neovim.nix
@@ -58,7 +58,7 @@
       catppuccin-nvim
     ];
     extraLuaConfig = ''
-      vim.cmd.colorscheme('catppuccin-latte')
+      vim.cmd.colorscheme('catppuccin-mocha')
       require'nvim-treesitter.configs'.setup {
         highlight = {
           enable = true,

59c33257f30e5a49076717d7316f39f57f48bedf
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2025-08-11 15:22:32 +0200

    fix(flake): adjust flake to use 'nh' on execution
    - when running 'nix run' use 'nh' package to apply configuration
    
    Resolves:
    Related:
    Signed-off-by: Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     };
   };
 
-  outputs = { self, nix-darwin, nixpkgs, home-manager, sops-nix }:
+  outputs = { self, nix-darwin, nixpkgs, home-manager, sops-nix, inputs }:
     let
       # Define darwinConfigurations first
       darwinConfigurations = {
@@ -61,17 +61,25 @@
       # Define packages for `nix build`
       packages.aarch64-darwin.default =
         let
-          pkgs = import nixpkgs { system = "aarch64-darwin"; };
+          pkgs = import nixpkgs { 
+            system = "aarch64-darwin";
+            overlays = [
+              # Add nh to the package set
+              (final: prev: {
+                nh = inputs.nh.packages.aarch64-darwin.default;
+              })
+            ];
+          };
         in
         pkgs.writeShellScriptBin "apply-configurations" ''
           USERNAME=$(whoami)
 
           if [ "$USERNAME" = "dminca" ]; then
-            sudo ${darwinConfigurations.ZionProxy.config.system.build.toplevel}/sw/bin/darwin-rebuild switch --flake . &&
-            ${homeConfigurations.dminca.activationPackage}/activate
+            ${pkgs.nh}/bin/nh os switch --hostname ZionProxy . &&
+            ${pkgs.nh}/bin/nh home switch --configuration dminca .
           elif [ "$USERNAME" = "mida4001" ]; then
-            sudo ${darwinConfigurations.MLGERHL6W4P2RXH.config.system.build.toplevel}/sw/bin/darwin-rebuild switch --flake . &&
-            ${homeConfigurations.mida4001.activationPackage}/activate
+            ${pkgs.nh}/bin/nh os switch --hostname MLGERHL6W4P2RXH . &&
+            ${pkgs.nh}/bin/nh home switch --configuration mida4001 .
           else
             echo "Unknown user: $USERNAME"
             exit 1

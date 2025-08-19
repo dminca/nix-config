d1816c30f71737ad514e3cfb312945fd1f9e6511
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2025-08-20 00:50:57 +0200

    feat: install packages
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -4,6 +4,34 @@
 }:
 {
   home.stateVersion = "23.11";
+  home.packages = with pkgs; [
+    ################
+    # core tooling #
+    ################
+    git-lfs
+    nil
+    nixfmt-classic
+    nodePackages.bash-language-server
+    nerd-fonts.jetbrains-mono
+    xh
+    tcptraceroute
+    doggo
+    #################
+    # shell tooling #
+    #################
+    go
+    sipcalc
+    hugo
+    openssl
+    operator-sdk
+    gum
+    yt-dlp
+    ########
+    # Apps #
+    ########
+    raycast
+    blender
+  ];
 }
 
 

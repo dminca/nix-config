93c4e974a8e94496274ee9ad2bee4e074a0a8504
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2025-08-20 00:30:21 +0200

    fix: set some sane defaults
    - install 'neovim'
    - enable 'zsh'
    
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- hosts/common/nixos-system.nix
+++ hosts/common/nixos-system.nix
@@ -1,2 +1,15 @@
-{}
+{
+  pkgs,
+  ...
+}:
+{
+  environment.systemPackages = with pkgs; [
+    neovim
+  ];
+  nix.settings.experimental-features = "nix-command flakes";
+  nix.enable = false;
+  programs.zsh.enable = true;
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToEscape = true;
+}
 

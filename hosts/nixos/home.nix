f5ac249e95a6cb540bf0730a33498546baf4f869
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2025-08-20 15:29:29 +0200

    fix: setup git, sops and go
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -1,4 +1,5 @@
 {
+  config,
   pkgs,
   ...
 }:
@@ -32,6 +33,22 @@
     ########
     blender
   ];
+  programs.home-manager.enable = true;
+  home.file = {
+    "${config.xdg.configHome}/git/git-commit-template.commit".source = ./dotfiles/git-commit-template.commit;
+    "${config.xdg.configHome}/yazi/theme.toml".source =
+      pkgs.fetchFromGitHub {
+        owner = "catppuccin";
+        repo = "yazi";
+        rev = "9bfdccc2b78d7493fa5c5983bc176a0bc5fef164";
+        sha256 = "sha256-a2X9WToZmctD1HZVqN9A512iPd+3dtjRloBEifgteF4=";
+      } + "/themes/mocha.toml";
+  };
+  home.sessionVariables = {
+    SOPS_AGE_KEY_FILE = "${config.xdg.configHome}/sops/age/keys.txt";
+  };
+  home.sessionPath = [
+    "$GOPATH/bin"
+  ];
 }
-
 

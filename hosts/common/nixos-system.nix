003b2486baeacc45d9429b85f68d704fb35afb8f
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2025-08-20 14:35:16 +0200

    fix: set etc nix config flags
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- hosts/common/nixos-system.nix
+++ hosts/common/nixos-system.nix
@@ -9,5 +9,21 @@
   nix.settings.experimental-features = "nix-command flakes";
   nix.enable = false;
   programs.zsh.enable = true;
+  environment.etc."nix/nix.conf".text = ''
+    allowed-users = *
+    auto-optimise-store = false
+    builders =
+    cores = 0
+    max-jobs = auto
+    require-sigs = true
+    sandbox = true
+    sandbox-fallback = false
+    substituters = https://cache.nixos.org/
+    system-features = nixos-test benchmark big-parallel kvm
+    trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
+    trusted-substituters =
+    trusted-users = root
+    extra-sandbox-paths =
+  '';
 }
 

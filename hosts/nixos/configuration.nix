4e225a6d937e88466ec95b68a6624d2ac56ee0bd
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2025-08-23 21:12:01 +0200

    feat(nixos): switch to i3wm
    - https://nixos.wiki/wiki/I3
    
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -46,8 +46,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.desktopManager.gnome.enable = true;
-  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = false;
+  services.displayManager.gdm.enable = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {

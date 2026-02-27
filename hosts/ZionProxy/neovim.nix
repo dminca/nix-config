80a51382a1060ba002b4e549e625b903061fc245
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2026-02-27 23:34:03 +0100

    feat(vim): show codeblocks better

--- hosts/ZionProxy/neovim.nix
+++ hosts/ZionProxy/neovim.nix
@@ -1,10 +1,23 @@
 {
+  pkgs,
   ...
 }:
 {
   programs.neovim = {
+    plugins = [pkgs.vimPlugins.mini-indentscope];
     initLua = ''
       vim.cmd.colorscheme('zaibatsu')
+      ---
+      local MiniIndentscope = require('mini.indentscope')
+      MiniIndentscope.setup(
+      {
+        draw = { delay = 0 },
+
+        options = {
+          indent_at_cursor = true,
+      },
+      symbol = '│',
+      })
     '';
   };
 }

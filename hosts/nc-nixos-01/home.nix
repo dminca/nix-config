d96bde151ab24d537d124b09eb0ab6404789a195
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2026-05-08 13:15:45 +0200

    fix: home stateVersion

--- hosts/nc-nixos-01/home.nix
+++ hosts/nc-nixos-01/home.nix
@@ -3,7 +3,7 @@
   ...
 }:
 {
-  home.stateVersion = "23.11";
+  home.stateVersion = "25.11";
   home.sessionVariables = {
     SOPS_AGE_KEY_FILE = "${config.xdg.configHome}/sops/age/keys.txt";
   };

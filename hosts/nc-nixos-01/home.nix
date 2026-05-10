e8edd4741b93fd3b3f9ddefa72f03f4684b362dc
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2026-05-10 10:22:07 +0200

    fix(nc-nixos-01): remove redundant sops flags
    * these are not even picked up

--- hosts/nc-nixos-01/home.nix
+++ hosts/nc-nixos-01/home.nix
@@ -4,15 +4,8 @@
 }:
 {
   home.stateVersion = "25.11";
-  home.sessionVariables = {
-    SOPS_AGE_KEY_FILE = "${config.xdg.configHome}/sops/age/keys.txt";
-  };
   sops = {
     defaultSopsFile = ./secrets/example.yaml;
     age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
-  };
-  sops.secrets.nextcloud = {
-    sopsFile = ./secrets/nextcloud.yaml;
-    key = "password";
   };
 }

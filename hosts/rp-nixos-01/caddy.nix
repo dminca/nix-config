005f7505a4768c8a6ce82b92dbc67cf3b9ab808d
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2026-05-10 08:53:16 +0200

    fix(caddy): test TLS certificate

--- hosts/rp-nixos-01/caddy.nix
+++ hosts/rp-nixos-01/caddy.nix
@@ -2,11 +2,31 @@
   ...
 }:
 {
+  sops.secrets.fullchain = {
+    sopsFile = ./secrets/certs.yaml;
+    key = "fullchain";
+    owner = "caddy";
+    group = "caddy";
+    path = "/var/lib/caddy/certs/fullchain.pem";
+  };
+  sops.secrets.privkey = {
+    sopsFile = ./secrets/certs.yaml;
+    key = "privkey";
+    owner = "caddy";
+    group = "caddy";
+    path = "/var/lib/caddy/certs/privkey.pem";
+    mode = "0400";
+  };
   services.caddy = {
     enable = true;
-    virtualHosts."localhost" = {
+    virtualHosts."test.mrbl.dedyn.io" = {
       extraConfig = ''
-        respond OK
+        test.mrbl.dedyn.io {
+            tls /var/lib/caddy/certs/fullchain.pem \
+                /var/lib/caddy/certs/privkey.pem
+
+            reverse_proxy localhost
+        }
       '';
     };
   };

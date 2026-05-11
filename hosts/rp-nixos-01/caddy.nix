65b0c26640b6e77f1ef68f566e85c68065e6b0f4
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2026-05-11 18:48:19 +0200

    fix(caddy): move config to NixOS LXC
    * Caddy configuration is now managed by NixOS in this LXC

--- hosts/rp-nixos-01/caddy.nix
+++ hosts/rp-nixos-01/caddy.nix
@@ -17,15 +17,58 @@
   };
   services.caddy = {
     enable = true;
-    virtualHosts."test.mrbl.dedyn.io" = {
-      extraConfig = ''
-        tls ${config.sops.secrets."fullchain.pem".path} \
-            ${config.sops.secrets."privkey.pem".path}
+    virtualHosts = {
+      "fw.mrbl.dedyn.io" = {
+        extraConfig = ''
+          tls ${config.sops.secrets."fullchain.pem".path} \
+              ${config.sops.secrets."privkey.pem".path}
 
-        reverse_proxy localhost
+          reverse_proxy 192.168.178.3
+        '';
+      };
+      "dns.mrbl.dedyn.io" = {
+        extraConfig = ''
+          tls ${config.sops.secrets."fullchain.pem".path} \
+              ${config.sops.secrets."privkey.pem".path}
 
-        respond OK
-      '';
+          reverse_proxy 192.168.178.2
+        '';
+      };
+      "nc.mrbl.dedyn.io" = {
+        extraConfig = ''
+          tls ${config.sops.secrets."fullchain.pem".path} \
+              ${config.sops.secrets."privkey.pem".path}
+
+          reverse_proxy 10.10.10.102
+        '';
+      };
+      "kc.mrbl.dedyn.io" = {
+        extraConfig = ''
+          tls ${config.sops.secrets."fullchain.pem".path} \
+              ${config.sops.secrets."privkey.pem".path}
+
+          reverse_proxy 10.10.10.118 {
+              header_up Host {host}
+              header_up X-Real-IP {remote}
+              header_up X-Forwarded-For {remote}
+              header_up X-Forwarded-Proto {scheme}
+              header_up X-Forwarded-Host {host}
+              header_up X-Forwarded-Port {http.request.port}
+          }
+        '';
+      };
+      "pve.mrbl.dedyn.io" = {
+        extraConfig = ''
+          tls ${config.sops.secrets."fullchain.pem".path} \
+              ${config.sops.secrets."privkey.pem".path}
+
+          reverse_proxy https://192.168.178.16:8006 {
+              transport http {
+                  tls_insecure_skip_verify
+              }
+          }
+        '';
+      };
     };
   };
 }

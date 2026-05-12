0ab36387a2595158630f4f18ebdb8d1e852b2e9e
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2026-05-12 12:21:23 +0200

    fix(arr): data volume mapping

--- hosts/md-nixos-01/arrstack.nix
+++ hosts/md-nixos-01/arrstack.nix
@@ -12,38 +12,37 @@
 	services.radarr = {
 		enable = true;
 		openFirewall = true;
-		dataDir = "/srv/appdata/radarr";
+		dataDir = "/mnt/arr-data/appdata/radarr";
 	};
 
 	services.sonarr = {
 		enable = true;
 		openFirewall = true;
-		dataDir = "/srv/appdata/sonarr";
+		dataDir = "/mnt/arr-data/appdata/sonarr";
 	};
 
 	services.bazarr = {
 		enable = true;
 		openFirewall = true;
-		dataDir = "/srv/appdata/bazarr";
+		dataDir = "/mnt/arr-data/appdata/bazarr";
 	};
 
 	services.jellyfin = {
 		enable = true;
 		openFirewall = true;
-		dataDir = "/srv/appdata/jellyfin";
+		dataDir = "/mnt/arr-data/appdata/jellyfin";
 	};
 
-	# /srv/media is intended for your mounted dataset or block device.
 	systemd.tmpfiles.rules = [
-		"d /srv/appdata 0755 root root -"
-		"d /srv/appdata/radarr 0750 radarr media -"
-		"d /srv/appdata/sonarr 0750 sonarr media -"
-		"d /srv/appdata/bazarr 0750 bazarr media -"
-		"d /srv/appdata/jellyfin 0750 jellyfin media -"
-		"d /srv/media 2775 root media -"
-		"d /srv/media/movies 2775 root media -"
-		"d /srv/media/tv 2775 root media -"
-		"d /srv/media/downloads 2775 root media -"
+		"d /mnt/arr-data/appdata 0755 root root -"
+		"d /mnt/arr-data/appdata/radarr 0750 radarr media -"
+		"d /mnt/arr-data/appdata/sonarr 0750 sonarr media -"
+		"d /mnt/arr-data/appdata/bazarr 0750 bazarr media -"
+		"d /mnt/arr-data/appdata/jellyfin 0750 jellyfin media -"
+		"d /mnt/arr-data/media 2775 root media -"
+		"d /mnt/arr-data/media/movies 2775 root media -"
+		"d /mnt/arr-data/media/tv 2775 root media -"
+		"d /mnt/arr-data/media/downloads 2775 root media -"
 	];
 }
 

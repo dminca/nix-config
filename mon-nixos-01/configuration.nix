a6770f4e94e4e642ad02bd9aa9ed09235809ee80
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2026-05-24 21:00:31 +0200

    feat: prepare control VM
    * this is where we're going to host the control machine for monitoring &
    logging

---
+++ mon-nixos-01/configuration.nix
@@ -0,0 +1,65 @@
+{
+  modulesPath,
+  lib,
+  pkgs,
+  ...
+}@args:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+  ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  networking.hostName = "mon-nixos-01";
+  nix.settings.trusted-users = [ "admin" ];
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+
+  # ── SSH ───────────────────────────────────────────────────────────────────
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  # ── Base packages ─────────────────────────────────────────────────────────
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.gitMinimal
+    pkgs.vim
+    pkgs.htop
+    pkgs.curl
+    pkgs.tmux
+  ];
+
+  # ── Users ─────────────────────────────────────────────────────────────────
+  # A single admin user is baked in.  Add your own public key here.
+  users.users.admin = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINuhmI6QfT3B6wMs7FaQClAtlEa2KHbW/fKFXvzE2+kX dminca@ZionProxy-2025-08-20"
+    ];
+  };
+
+  # ── Misc ──────────────────────────────────────────────────────────────────
+  time.timeZone = "UTC";
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+  ];
+  i18n.defaultLocale = "en_US.UTF-8";
+  services.qemuGuest.enable = true;
+
+  system.stateVersion = "25.11";
+}

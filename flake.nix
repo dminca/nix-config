633fab8ee935b55b8a5bfd60c3cd53bc2a24b15f
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2026-05-31 19:48:13 +0200

    chore: flake update to 26.05

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Daniel personal workstation";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {

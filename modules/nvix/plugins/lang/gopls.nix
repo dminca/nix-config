a8bb671b2a9d70cfe630b84b1989c873df8a85fd
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2026-06-17 11:03:51 +0200

    fix(nvix): extend Go support
    * enable formatting
    * enable imports optimization (on save)

--- modules/nvix/plugins/lang/gopls.nix
+++ modules/nvix/plugins/lang/gopls.nix
@@ -1,5 +1,41 @@
+{ lib, pkgs, ... }:
 {
   plugins = {
-    lsp.servers.gopls.enable = true;
+    lsp.servers.gopls = {
+      enable = true;
+      settings = {
+        gopls = {
+          # Organize imports on save
+          gofumpt = true;
+          usePlaceholders = true;
+          completeUnimported = true;
+          semanticTokens = true;
+          staticcheck = true;
+        };
+      };
+    };
+    conform-nvim.settings = {
+      formatters_by_ft.go = [ "goimports" ];
+      formatters.goimports = {
+        command = lib.getExe' pkgs.gotools "goimports";
+      };
+    };
   };
+
+  # Auto-format on save for Go files
+  autoGroups.gopls_format = {
+    clear = true;
+  };
+  autoCmd = [
+    {
+      group = "gopls_format";
+      event = [ "BufWritePre" ];
+      pattern = [ "*.go" ];
+      callback.__raw = ''
+        function()
+          require('conform').format({ async = false })
+        end
+      '';
+    }
+  ];
 }

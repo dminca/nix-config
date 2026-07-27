ce7e2ee7b4c2f30e5189d363491e713990c3ea36
Daniel-Andrei Minca <zealous_sterner.0e@icloud.com>
2026-07-27 11:15:04 +0200

    feat: enable 'npm'

--- hosts/MLGERHL6W4P2RXH/home.nix
+++ hosts/MLGERHL6W4P2RXH/home.nix
@@ -296,6 +296,9 @@
       target = "${config.home.homeDirectory}/Notes/my-vault";
     };
   };
+  programs.npm = {
+    enable = true;
+  };
 
   home.activation.createObsidianVault = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     mkdir -p ${config.home.homeDirectory}/Notes/my-vault

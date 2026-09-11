71d7ece5e4eb9041905bd901ae57f35147ae4369
Daniel-Andrei Minca <odd.wire1942@fastmail.com>
2026-09-11 15:43:10 +0200

    fix(polybar): reduce emoji font size to 8pt to prevent oversizing

--- modules/home-manager/polybar/default.nix
+++ modules/home-manager/polybar/default.nix
@@ -87,7 +87,7 @@ in
       module-margin = 1
       separator = " | "
       font-0 = "JetBrains Mono:style=Regular:size=11;3"
-      font-1 = "Noto Color Emoji:style=Regular:size=10;3"
+      font-1 = "Noto Color Emoji:style=Regular:size=6;3"
       modules-left = i3 xwindow
       modules-center = clock
       modules-right = layout volume battery network cpu memory tray
